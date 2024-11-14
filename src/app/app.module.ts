85cb2758790c33b3930a5f4b1ed02c265bd2ec17
Lucas <lucasvieirasouzaa@gmail.com>
2024-11-14 17:19:34 -0300

    layout update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DownloadContainerComponent } from './download-container/download-container.component';
-import { MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { HeaderComponent } from './header/header.component';
 import { TextContainerComponent } from './text-container/text-container.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatButton } from '@angular/material/button';
 
 @NgModule({
-  declarations: [			
+  declarations: [
     AppComponent,
     DownloadContainerComponent,
-      HeaderComponent,
-      TextContainerComponent
-   ],
+    HeaderComponent,
+    TextContainerComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTooltip,
+    MatButton,
+    ReactiveFormsModule
   ],
   providers: [
     provideAnimationsAsync('noop'),
